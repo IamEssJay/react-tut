a35f8ecfbb70284470d085c9563adb6bfa8b3b2a
IamEssJay <emmasj2019@gmail.com>
2023-03-01 11:50:11 +0000

    day2a

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,36 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 const Home = () => {
+    const [blogs, setBlogs] = useState(null);
+    const [IsLoading, setIsLoading]= useState(true);
+    const [error, setEror]= useState(null);
+
+   
+    useEffect(()=>{
+        setTimeout(()=>{
+            fetch('http://localhost:8000/blogs')
+        .then(res =>{
+            if(!res.ok){
+                throw Error("Can't fetch data");
+            }
+            return res.json();
+        })
+        .then(data =>{
+            setBlogs(data);
+            setIsLoading(false);
+            setEror(null)
+        }).catch((e)=>{
+            setIsLoading(false);
+            setEror(e.message)
+        })
+        }, 1000)
+    }, []);
+
     return ( 
          <div className="home">
-            <h2>HomePage</h2>
+            {error && <div>{error}</div>}
+            {IsLoading && <div>Loading...</div>}
+           {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
          </div>
      );
 }
